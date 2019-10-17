8cd7aad01ab7a538b9dd841ec1793b5ffee7fcbf
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-10-16 21:43:55 -0700

    random number guess 1-6

--- random-number-guess-th/script.js
+++ random-number-guess-th/script.js
@@ -1,8 +1,25 @@
+//Method-1
+
+var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 6) + 1;
 var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
 if (parseInt(guess) === randomNumber) {
-	document.write('<h2>Congratulations! You guessed the number!</h2>');
+	document.write('<h2>You gueseed the number!</h2>');
+}else {
+	document.write('<h2>Sorry. The number was ' + randomNumber + '</h2>');
+}
+
+//Method-2, using boolean variables false/true
+
+var correctGuess = false;
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
+if (parseInt(guess) === randomNumber) {
+	correctGuess = true;
+}
+if ( correctGuess ) {
+	document.write('<h2>You gueseed the number!</h2>');
 } else {
-		document.write('<h2>Sorry. The number was ' + randomNumber + '</h2>');
+	document.write('<h2>Sorry. The number was ' + randomNumber + '</h2>');
 }
 									 
