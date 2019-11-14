67080183c1068f36eb7429434ea6a1e63d5a94e5
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-11-14 10:01:33 -0800

    some additional js files

--- do-while-loop/js/script.js
+++ do-while-loop/js/script.js
@@ -1,21 +1,35 @@
-var randomNumber = getRandomNumber(1);
-var guess;
-var guessCount = 0;
-var correctGuess = false;
+// 01
 
-function getRandomNumber( upper ) {
-  var num = Math.floor(Math.random() * upper) + 1; 
-  return num;
+// var randomNumber = getRandomNumber(1);
+// var guess;
+// var guessCount = 0;
+// var correctGuess = false;
+
+// function getRandomNumber( upper ) {
+//   var num = Math.floor(Math.random() * upper) + 1; 
+//   return num;
+// }
+
+// do {
+//   guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+//   guessCount += 1;
+//   if (parseInt(guess) === randomNumber) {
+//     correctGuess = true;
+//   }
+// } while ( ! correctGuess ) 
+//   document.write('<h1>You guessed the number!</h1>');
+// document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+
+// #02
+
+// var secret;
+// do {
+//   secret = prompt("What is the secret password?");    
+// } while (secret !== "sesame") {
+//   document.write("You know the secret password. Welcome!");
+// }
+
+
+for (var i=1; i<=11; i+=2) {
+  console.log(i)
 }
-
-do {
-  guess = prompt('I am thinking of a number between 1 and 10. What is it?');
-  guessCount += 1;
-  if (parseInt(guess) === randomNumber) {
-    correctGuess = true;
-  }
-} while ( ! correctGuess ) 
-  document.write('<h1>You guessed the number!</h1>');
-document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
-
-
