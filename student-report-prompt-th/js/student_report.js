abc525c612fa0806ca4faa9492af159ea0e5b8a4
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-11-15 22:52:18 -0800

    adds different folders

---
+++ student-report-prompt-th/js/student_report.js
@@ -0,0 +1,31 @@
+var message = '';
+var student;
+var search;
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+function getStudentReport( student ) {
+  var report = '<h2>Student: ' + student.name + '</h2>';
+  report += '<p>Track: ' + student.track + '</p>';
+  report += '<p>Points: ' + student.points + '</p>';
+  report += '<p>Achievements: ' + student.achievements + '</p>';
+  return report;
+}
+
+while (true) {
+  search = prompt('Search student records: type a name [Jody] (or type "quit" to end)');
+  if (search.toLowerCase() === 'quit' || search === null) {
+    break;
+  }
+  for (var i = 0; i < students.length; i += 1) {
+    student = students[i];
+    if ( student.name.toLowerCase() === search.toLowerCase() ) {
+      message = getStudentReport( student );
+      print(message);
+    }
+  }
+}
+
